33fd4ba0d50b3913a69b7995fea3b45b273e44e6
kbasma <katia.bazzi@gmail.com>
2015-04-05 21:50:46 -0500

    changed yummly api model to accomodate view

--- app/assets/javascripts/yummly_api.js
+++ app/assets/javascripts/yummly_api.js
@@ -1,15 +1,27 @@
-
 var YummlyApi = function() {
     var keyAndId = new ApiKeyID();
     this.apiKey = keyAndId[0];
     this.apiID = keyAndId[1];
     this.firstResponse = null;
+    this.maxItems = 20;
+    this.start = 0;
 };
 
 
-YummlyApi.prototype.search = function (start) {
+YummlyApi.prototype.fetchRecipes = function() {
+    var ajaxCall = $.ajax({
+      url: "http://api.yummly.com/v1/api/recipes?_app_id=" + this.apiID + "&_app_key=" + this.apiKey + "&requirePictures=true" + "&maxResult=" + this.maxItems + "&start=" + this.start,
+      method: "GET",
+      dataType: "json"
+    });
+    this.start += this.maxItems;
+    return ajaxCall
+}
+
+
+YummlyApi.prototype.search = function (start, query) {
     $.ajax({
-      url: "http://api.yummly.com/v1/api/recipes?_app_id=" + this.apiID + "&_app_key=" + this.apiKey + "&q=onion+soup" + "&requirePictures=true" + "&maxResult=10&start=" + start,
+      url: "http://api.yummly.com/v1/api/recipes?_app_id=" + this.apiID + "&_app_key=" + this.apiKey + "&q=" +query + "&requirePictures=true" + "&maxResult=10&start=" + this.start,
       method: "GET",
       dataType: "json"
     })
@@ -19,35 +31,11 @@ YummlyApi.prototype.search = function (start) {
 };
 
 YummlyApi.prototype.getRecipe = function (recipeID) {
-    $.ajax({
+    return $.ajax({
       url: "http://api.yummly.com/v1/api/recipe/"+ recipeID + "?_app_id=" + this.apiID + "&_app_key=" + this.apiKey,
       method: "GET",
       dataType: "json"
-    })
-      .done(function(data) {
-        this.firstResponse = data;
-    }.bind(this));
+    });
     
 };
-
-YummlyApi.prototype.randomRecipes = function() {
-    $.ajax({
-      url: "http://api.yummly.com/v1/api/recipes?_app_id=" + this.apiID + "&_app_key=" + this.apiKey + "&requirePictures=true",
-      method: "GET",
-      dataType: "json"
-    })
-      .done(function(data) {
-        this.firstResponse = data;
-    }.bind(this));
-}
-
-
-YummlyApi.prototype.fetchMore = function() {
-
-
-}
-
-
-
-var yummlyApi = new YummlyApi();
 
