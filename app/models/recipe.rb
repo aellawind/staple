1dcaf72e08d98f502ef2c032c6ae80daaad93264
kbasma <katia.bazzi@gmail.com>
2015-04-11 11:46:50 -0500

    added uniqueness validation to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
   validates :name, presence: true
-  validates :yummly_id, presence: true
+  validates :yummly_id, presence: true, uniqueness: true
   belongs_to :user
 end
